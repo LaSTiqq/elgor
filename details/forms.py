562e0b2b22c855a484acc2a030ec5df43692ce37
LaSTiqq <lavrencij13@gmail.com>
2023-07-28 22:05:23 +0300

    Added Google Recaptcha

--- details/forms.py
+++ details/forms.py
@@ -1,24 +1,26 @@
 from django import forms
-# from django.conf import settings
-# from captcha.fields import ReCaptchaField
-# from captcha.widgets import ReCaptchaV2Checkbox
+from django.conf import settings
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Invisible
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your name', widget=forms.TextInput(attrs={
+    name = forms.CharField(label='Name', widget=forms.TextInput(attrs={
                            'class': 'form-control my-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'required': True}))
-    sender = forms.EmailField(label='Your email', widget=forms.EmailInput(
+    sender = forms.EmailField(label='Email', widget=forms.EmailInput(
         attrs={'class': 'form-control my-1', 'placeholder': 'Your email *', 'autocomplete': 'off', 'required': True}))
-    subject = forms.CharField(label='Message topic', widget=forms.TextInput(
+    subject = forms.CharField(label='Topic', widget=forms.TextInput(
         attrs={'class': 'form-control my-1', 'placeholder': 'Message topic *', 'autocomplete': 'off', 'required': True}))
-    content = forms.CharField(label='Message text', widget=forms.Textarea(
+    content = forms.CharField(label='Text', widget=forms.Textarea(
         attrs={'class': 'form-control my-1', 'rows': 10, 'placeholder': 'Message text *', 'autocomplete': 'off', 'required': True}))
-    # captcha = ReCaptchaField(
-    #     label='Captcha',
-    #     widget=ReCaptchaV2Checkbox(
-    #         attrs={
-    #             'style': 'display: flex; justify-content: center; margin-top: 0.5rem;',
-    #             'data-sitekey': settings.RECAPTCHA_PUBLIC_KEY,
-    #         },
-    #     ),
-    # )
+    captcha = ReCaptchaField(
+        label='Captcha',
+        widget=ReCaptchaV2Invisible(
+            attrs={
+                'data-sitekey': settings.RECAPTCHA_PUBLIC_KEY,
+            },
+            api_params={
+                'hl': 'en',
+            },
+        ),
+    )
