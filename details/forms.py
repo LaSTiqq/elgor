874af29e33cc14fff2acc8e7181b9d9bbae78615
LaSTiqq <lavrencij13@gmail.com>
2024-03-28 21:59:43 +0200

    Some email HTML changes #2

--- details/forms.py
+++ details/forms.py
@@ -6,13 +6,13 @@ from captcha.widgets import ReCaptchaV3
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', widget=forms.TextInput(attrs={
-                           'class': 'form-control my-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'required': True, 'maxlength': '15'}))
-    sender = forms.EmailField(label='Email', widget=forms.EmailInput(
-        attrs={'class': 'form-control my-1', 'placeholder': 'Your email *', 'autocomplete': 'off', 'required': True}))
-    subject = forms.CharField(label='Topic', widget=forms.TextInput(
-        attrs={'class': 'form-control my-1', 'placeholder': 'Message topic *', 'autocomplete': 'off', 'required': True, 'maxlength': '30'}))
-    content = forms.CharField(label='Text', widget=forms.Textarea(
-        attrs={'class': 'form-control my-1', 'rows': 10, 'placeholder': 'Message text *', 'autocomplete': 'off', 'required': True}), min_length=50)
+        'class': 'form-control my-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'maxlength': '15'}))
+    sender = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
+        'class': 'form-control my-1', 'placeholder': 'Your email *', 'autocomplete': 'off'}))
+    subject = forms.CharField(label='Topic', widget=forms.TextInput(attrs={
+        'class': 'form-control my-1', 'placeholder': 'Message topic *', 'autocomplete': 'off', 'maxlength': '30'}))
+    content = forms.CharField(label='Text', widget=forms.Textarea(attrs={
+        'class': 'form-control my-1', 'placeholder': 'Message text *', 'autocomplete': 'off', 'rows': '10'}), min_length=50)
     captcha = ReCaptchaField(
         label='Captcha',
         widget=ReCaptchaV3(
