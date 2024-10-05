c43845476bb8f39ab542e48fd21015f6f3ef587c
LaSTiqq <lavrencij13@gmail.com>
2024-10-05 23:48:01 +0300

    SEO update and style rework

--- details/forms.py
+++ details/forms.py
@@ -6,7 +6,7 @@ from captcha.widgets import ReCaptchaV3
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', widget=forms.TextInput(attrs={
-        'class': 'form-control my-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'maxlength': '15'}),
+        'class': 'form-control mb-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'maxlength': '15'}),
         min_length=5)
     sender = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
         'class': 'form-control my-1', 'placeholder': 'Your email *', 'autocomplete': 'off'}))
@@ -14,7 +14,7 @@ class ContactForm(forms.Form):
         'class': 'form-control my-1', 'placeholder': 'Message topic *', 'autocomplete': 'off', 'maxlength': '30'}),
         min_length=5)
     content = forms.CharField(label='Text', widget=forms.Textarea(attrs={
-        'class': 'form-control my-1', 'placeholder': 'Message text *', 'autocomplete': 'off', 'rows': 10}),
+        'class': 'form-control mt-1', 'placeholder': 'Message text *', 'autocomplete': 'off', 'rows': 6}),
         min_length=50)
     captcha = ReCaptchaField(
         label='Captcha',
