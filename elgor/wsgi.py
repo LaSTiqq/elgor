219b1e2a18bdefee8c59189c2be93712e747d2ae
LaSTiqq <lavrencij13@gmail.com>
2024-11-13 23:01:05 +0200

    Major upgrade to Django 4.2.16

--- elgor/wsgi.py
+++ elgor/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elgor.settings')
 
 application = get_wsgi_application()
