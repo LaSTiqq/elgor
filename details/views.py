c84b60b2bd747c32a85eae4930fb22a15922ed42
LaSTiqq <lavrencij13@gmail.com>
2025-01-29 23:42:07 +0200

    Get rid of access to sensitive info

--- details/views.py
+++ details/views.py
@@ -6,26 +6,7 @@ from django.utils.html import strip_tags
 from django.conf import settings
 from smtplib import SMTPException
 from .forms import ContactForm
-import re
-
-
-def restricted_found(text):
-    url_pattern = re.compile(
-        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    restricted_keywords = ["whatsapp", "telegram", "tg", "телеграм", "телега", "тг",
-                           "viber", "вайбер", "discord", "дискорд", "аська", "icq",
-                           "porn", "loli", "xxx", "cp", "skype", "скайп", "rub",
-                           "рублей", "руб", "dollars", "eur", "bonus", "free",
-                           "gift", "order now", "spam", "website", "visit our",
-                           "earn", "congratulations", "don't miss", "buy now",
-                           "limited time", "exclusive offer", "act fast", "special deal",
-                           "discount", "sale", "promotion", "pharmacy", "election", "price"]
-
-    has_link = bool(re.search(url_pattern, text))
-    has_restricted_keyword = any(re.search(
-        r'\b' + re.escape(keyword) + r'\b', text, flags=re.IGNORECASE) for keyword in restricted_keywords)
-
-    return has_link or has_restricted_keyword
+from .utils import restricted_found
 
 
 def send(request):
@@ -54,7 +35,7 @@ def send(request):
                 messages.success(request, "Message sent")
                 return redirect('/#contacts')
             except SMTPException:
-                messages.danger(
+                messages.warning(
                     request, "Something went wrong! Try again.")
                 return redirect('/#contacts')
         else:
