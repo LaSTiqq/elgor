ef252708ca989bcdc8e58d852c3d9326dafa24fb
LaSTiqq <lavrencij13@gmail.com>
2025-06-23 00:13:20 +0300

    Rework of form

--- details/forms.py
+++ details/forms.py
@@ -6,16 +6,16 @@ from django_recaptcha.widgets import ReCaptchaV3
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', widget=forms.TextInput(attrs={
-        'class': 'form-control mb-1', 'placeholder': 'Your name *', 'autocomplete': 'off', 'maxlength': '15'}),
-        min_length=5)
+        'id': 'name', 'value': 'John', 'class': 'form-control', 'placeholder': 'Name', 'autocomplete': 'off', 'maxlength': '15'}),
+        min_length=3)
     sender = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
-        'class': 'form-control my-1', 'placeholder': 'Your email *', 'autocomplete': 'off'}))
+        'id': 'email', 'value': 'john.doe@gmail.com', 'class': 'form-control', 'placeholder': 'Email', 'autocomplete': 'off'}))
     subject = forms.CharField(label='Topic', widget=forms.TextInput(attrs={
-        'class': 'form-control my-1', 'placeholder': 'Message topic *', 'autocomplete': 'off', 'maxlength': '30'}),
+        'id': 'subject', 'value': 'Collaboration', 'class': 'form-control', 'placeholder': 'Topic', 'autocomplete': 'off', 'maxlength': '30'}),
         min_length=5)
-    content = forms.CharField(label='Text', widget=forms.Textarea(attrs={
-        'class': 'form-control mt-1', 'placeholder': 'Message text *', 'autocomplete': 'off', 'rows': 6}),
-        min_length=50)
+    content = forms.CharField(label='Message', widget=forms.Textarea(attrs={
+        'id': 'message', 'class': 'form-control', 'placeholder': 'Message', 'autocomplete': 'off'}),
+        min_length=20)
     captcha = ReCaptchaField(
         label='Captcha',
         widget=ReCaptchaV3(
